e1abb1f6813b245f9f8a66bbadc45ce379663be6
Raflizar Deswandi Yahya <raflizardeswandiy24@gmail.com>
2022-12-26 14:42:25 +0700

    make portfolio & testimonial section

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import "./App.css";
 import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Qualification from "./components/qualification/Qualification";
 import Services from "./components/services/Services";
 import Skills from "./components/skills/Skills";
+import Testimonials from "./components/testimonials/Testimonials";
+import Work from "./components/work/Work";
 
 function App() {
 	return (
@@ -14,7 +17,10 @@ function App() {
 				<About />
 				<Skills />
 				<Services />
+				<Qualification />
+				<Work />
 			</main>
+			<Testimonials />
 		</>
 	);
 }
