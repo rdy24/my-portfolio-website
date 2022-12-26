51ed7862b3c2315261c133e6fbcc2741d31d6e30
Raflizar Deswandi Yahya <raflizardeswandiy24@gmail.com>
2022-12-26 09:00:26 +0700

    make skills & about

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
+import About from "./components/about/About";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Skills from "./components/skills/Skills";
 
 function App() {
 	return (
@@ -8,6 +10,8 @@ function App() {
 			<Header />
 			<main className="main">
 				<Home />
+				<About />
+				<Skills />
 			</main>
 		</>
 	);
